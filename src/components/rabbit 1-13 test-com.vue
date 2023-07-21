95832f1fffcbc509a0b4a0af979bc2b1a453f807
沈 <z212200228@163.com>
2023-07-21 16:14:00 +0800

    1-13

---
+++ src/components/rabbit 1-13 test-com.vue
@@ -0,0 +1,41 @@
+<!-- 1.13: 模板引用 -->
+<!-- 
+  
+ -->
+
+<template>
+  <div>
+    
+    <div>我是test组件</div>
+    <button @click="setName">修改name</button>
+
+ 
+  </div>
+</template>
+
+<!-- ==== script ==== -->
+<script setup>
+
+import { ref } from 'vue';
+
+const name = ref('test name')
+const setName = ()=> {
+  name.value = 'test new name'
+}
+
+/**
+ * 
+  默认情况下，在<script setup>里，组件内部的属性与方法是不开放给父组件访问的
+  使用 defineExpose编译宏 指定哪些属性和方法是允许访问的
+ * 
+*/ 
+defineExpose({
+  name,setName
+
+})
+</script>
+
+
+<!-- ==== style ==== -->
+<style scoped lang="scss">
+</style>
--- src/components/rabbit 1-13.vue
+++ src/components/rabbit 1-13.vue
@@ -1,5 +1,8 @@
 <!-- 1.13: 模板引用 -->
 <!-- 
+  == 总结 ==
+  1.获取模板引用的时机，在组件挂载完毕之后
+  2.defineExpose编译宏可以显式保暴露组件内部的属性和方法
   
  -->
 
@@ -8,20 +11,46 @@
     <!-- 2.通过ref标识绑定ref对象 -->
     <h1 ref="h1Ref">我是dom标签h1</h1>
 
+    <button  class="color" @click="setColor">设置颜色</button>
+
+
+    <TestCom ref="comRef" />
+
  
   </div>
 </template>
 
 <!-- ==== script ==== -->
 <script setup>
+import TestCom from './rabbit 1-13 test-com.vue'
 
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 // 1.调用ref函数得到ref对象
 const h1Ref = ref(null)
+const comRef = ref(null)
+
+// 组件挂载完毕才能获取
+onMounted( ()=> {
+  console.log(h1Ref.value)
+  console.log(comRef.value)
+  
+})
+
+// == 尝试修改颜色 ==
+const style = ref('dodgerblue')
+const setColor = ()=> {
+  style.value = 'pink'
+
+
+}
 
 </script>
 
 
 <!-- ==== style ==== -->
 <style scoped lang="scss">
+.color{  
+  background-color: dodgerblue;
+  background-color: v-bind(style);
+}
 </style>
